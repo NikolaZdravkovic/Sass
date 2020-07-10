ec3742826b0abd37d3efa8b64838702c1547ee47
Nikola Zdravkovic <34869917+NikolaZdravkovic@users.noreply.github.com>
2020-07-10 12:39:07 +0200

    Add slider for mobile
    Add slider for 376px

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ function remove(selector, cls) {
 
 function show(pos) {
     remove('.slide.showing', 'showing');
+    remove('.createdByText.show', 'show');
+    remove('.box4text.showText', 'showText');
+
+
+
     add('.slide:nth-child(' + pos + ')', 'showing')
+    add('.createdByText:nth-child(' + pos + ')', 'show')
+    add('.box4text:nth-child(' + pos + ')', 'showText')
+
+
     remove('.dotDesktop.active', 'active');
     add('.dotDesktop:nth-child(' + pos + ')', 'active')
 }
